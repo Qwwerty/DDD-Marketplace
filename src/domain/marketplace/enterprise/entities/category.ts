3c04507d85229b7a676e33a9ba5857acb783bfb7
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2025-05-21 13:07:09 -0300

    feat: list all categories use case

---
+++ src/domain/marketplace/enterprise/entities/category.ts
@@ -0,0 +1,21 @@
+import { UniqueEntityId } from '@/core/entities/unique-entidy-id'
+import { Entity } from '@/core/entities/entity'
+
+interface CategoryProps {
+  title: string
+  slug: string
+}
+
+export class Category extends Entity<CategoryProps> {
+  get title() {
+    return this.props.title
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  static create(props: CategoryProps, id?: UniqueEntityId): Category {
+    return new Category(props, id)
+  }
+}
