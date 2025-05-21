f4e811196ae8cbf75c26212787da494febe4817a
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2025-05-20 23:25:21 -0300

    feat: upload attachments use case

---
+++ src/domain/marketplace/application/use-cases/upload-attachments.ts
@@ -0,0 +1,65 @@
+import { Either, left, right } from '@/core/either'
+
+import { AttachmentsRepository } from '../repositories/attachmenets-repository'
+import { Attachment } from '../../enterprise/entities/attachment'
+import { Uploader } from '../storage/uploader'
+import { InvalidAttachemntTypeError } from './errors/invalid-attachment-error'
+
+interface IFile {
+  fileName: string
+  fileType: string
+  body: Buffer
+}
+
+interface UploadAttachmentsUseCaseProps {
+  files: IFile[]
+}
+
+type UploadAttachmentsUseCaseResponse = Either<
+  InvalidAttachemntTypeError,
+  {
+    attachments: Attachment[]
+  }
+>
+
+export class UploadAttachmentsUseCase {
+  constructor(
+    private attachmentsRepository: AttachmentsRepository,
+    private uploader: Uploader,
+  ) {}
+
+  async execute({
+    files,
+  }: UploadAttachmentsUseCaseProps): Promise<UploadAttachmentsUseCaseResponse> {
+    const attachmentInvalid = files.find(
+      (file) => !/^(image\/(jpeg|png))$|^application\/pdf$/.test(file.fileType),
+    )
+
+    if (attachmentInvalid) {
+      return left(new InvalidAttachemntTypeError(attachmentInvalid.fileType))
+    }
+
+    const uploadAtacchmentsPromises = files.map((file) =>
+      this.uploader.upload({
+        fileName: file.fileName,
+        fileType: file.fileType,
+        body: file.body,
+      }),
+    )
+
+    const result = await Promise.all(uploadAtacchmentsPromises)
+
+    const attachments = result.map((file) =>
+      Attachment.create({
+        title: file.fileName,
+        path: file.path,
+      }),
+    )
+
+    await this.attachmentsRepository.createMany(attachments)
+
+    return right({
+      attachments,
+    })
+  }
+}
