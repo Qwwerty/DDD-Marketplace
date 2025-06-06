aead9177ee37e0a36db2f210d3fa3f0d90c60d2a
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2025-06-05 23:46:45 -0300

    feat: mark sell as available use case

---
+++ src/domain/marketplace/application/use-cases/mark-sell-as-available.spec.ts
@@ -0,0 +1,160 @@
+import { UniqueEntityId } from '@/core/entities/unique-entidy-id'
+
+import { InMemoryProductAttachmentsRepository } from 'test/repositories/in-memory-product-attachments-repository'
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
+import { InMemoryProductsRepository } from 'test/repositories/in-memory-products-repository'
+import { makeProduct } from 'test/factories/make-product'
+
+import { makeSeller } from 'test/factories/make-seller'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { MarkSellAsAvailableUseCase } from './mark-sell-as-available'
+import { ProductStatus } from '../../enterprise/entities/product'
+import { InMemorySellersRepository } from 'test/repositories/in-memory-sellers-repository'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+let inMemoryProductAttachments: InMemoryProductAttachmentsRepository
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let inMemorySellersRepository: InMemorySellersRepository
+let inMemoryProductsRepository: InMemoryProductsRepository
+let sut: MarkSellAsAvailableUseCase
+
+describe('Mark Sell As Avaiable Use Case', () => {
+  beforeEach(() => {
+    inMemoryProductAttachments = new InMemoryProductAttachmentsRepository()
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    inMemoryProductsRepository = new InMemoryProductsRepository(
+      inMemoryProductAttachments,
+      inMemoryAttachmentsRepository,
+    )
+
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    inMemorySellersRepository = new InMemorySellersRepository(
+      inMemoryAttachmentsRepository,
+    )
+
+    sut = new MarkSellAsAvailableUseCase(
+      inMemoryProductsRepository,
+      inMemorySellersRepository,
+    )
+  })
+
+  it('should not updating the product status with a nonexistent user', async () => {
+    const seller = makeSeller({}, new UniqueEntityId('seller-1'))
+
+    const product = makeProduct(
+      {
+        title: 'product title',
+        description: 'product description',
+        status: ProductStatus.CANCELLED,
+      },
+      seller,
+      new UniqueEntityId('product-1'),
+    )
+
+    inMemoryProductsRepository.items.push(product)
+
+    const result = await sut.execute({
+      productId: 'product-1',
+      ownerId: seller.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not updating the status of a nonexistent product', async () => {
+    const seller = makeSeller({}, new UniqueEntityId('seller-1'))
+
+    const result = await sut.execute({
+      productId: 'product-1',
+      ownerId: seller.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not updating the status of product owned by another user', async () => {
+    const seller1 = makeSeller({}, new UniqueEntityId('seller-1'))
+    const seller2 = makeSeller({}, new UniqueEntityId('seller-2'))
+
+    inMemorySellersRepository.items.push(seller1, seller2)
+
+    const product = makeProduct(
+      {
+        title: 'product title',
+        description: 'product description',
+        status: ProductStatus.CANCELLED,
+      },
+      seller1,
+      new UniqueEntityId('product-1'),
+    )
+
+    inMemoryProductsRepository.items.push(product)
+
+    const result = await sut.execute({
+      productId: 'product-1',
+      ownerId: 'seller-2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+
+  it('should not allow marking a sold productt as available', async () => {
+    const seller1 = makeSeller({}, new UniqueEntityId('seller-1'))
+
+    inMemorySellersRepository.items.push(seller1)
+
+    const product = makeProduct(
+      {
+        title: 'product title',
+        description: 'product description',
+        status: ProductStatus.SOLD,
+      },
+      seller1,
+      new UniqueEntityId('product-1'),
+    )
+
+    inMemoryProductsRepository.items.push(product)
+
+    const result = await sut.execute({
+      productId: 'product-1',
+      ownerId: 'seller-1',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+
+  it('should allow updating the product status to available', async () => {
+    const seller = makeSeller({}, new UniqueEntityId('seller-1'))
+
+    inMemorySellersRepository.items.push(seller)
+
+    const product = makeProduct(
+      {
+        title: 'product title',
+        description: 'product description',
+        status: ProductStatus.CANCELLED,
+      },
+      seller,
+      new UniqueEntityId('product-1'),
+    )
+
+    inMemoryProductsRepository.items.push(product)
+
+    const result = await sut.execute({
+      productId: 'product-1',
+      ownerId: seller.id.toString(),
+    })
+
+    expect(result.value).toStrictEqual(
+      expect.objectContaining({
+        product: expect.objectContaining({
+          status: 'available',
+        }),
+      }),
+    )
+  })
+})
---
+++ src/domain/marketplace/application/use-cases/mark-sell-as-available.ts
@@ -0,0 +1,58 @@
+import { Either, left, right } from '@/core/either'
+import { ProductsRepository } from '../repositories/products-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { Product, ProductStatus } from '../../enterprise/entities/product'
+import { SellersRepository } from '../repositories/sellers-repository'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+interface MarkSellAsAvaiableUseCaseRequest {
+  productId: string
+  ownerId: string
+}
+
+type MarkSellAsAvaiableUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    product: Product
+  }
+>
+
+export class MarkSellAsAvailableUseCase {
+  constructor(
+    private productsRepository: ProductsRepository,
+    private sellersRepository: SellersRepository,
+  ) {}
+
+  async execute({
+    productId,
+    ownerId,
+  }: MarkSellAsAvaiableUseCaseRequest): Promise<MarkSellAsAvaiableUseCaseResponse> {
+    const seller = await this.sellersRepository.findById(ownerId)
+
+    if (!seller) {
+      return left(new ResourceNotFoundError('ownerId', ownerId))
+    }
+
+    const product = await this.productsRepository.findById(productId)
+
+    if (!product) {
+      return left(new ResourceNotFoundError('productId', productId))
+    }
+
+    if (product.onwer.id.toString() !== ownerId) {
+      return left(new NotAllowedError())
+    }
+
+    if (product.status === ProductStatus.SOLD) {
+      return left(new NotAllowedError())
+    }
+
+    product.status = ProductStatus.AVAILABLE
+
+    this.productsRepository.save(product)
+
+    return right({
+      product,
+    })
+  }
+}
