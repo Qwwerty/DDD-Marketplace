29d14399700ee80e144e8cd3b19f7f2193f10e0f
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2025-06-07 08:20:38 -0300

    feat: mark sell as sold use case

---
+++ src/domain/marketplace/application/use-cases/mark-sell-as-sold.ts
@@ -0,0 +1,59 @@
+import { Either, left, right } from '@/core/either'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+import { ProductsRepository } from '../repositories/products-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { Product, ProductStatus } from '../../enterprise/entities/product'
+import { SellersRepository } from '../repositories/sellers-repository'
+
+interface MarkSellAsSolddUseCaseRequest {
+  productId: string
+  ownerId: string
+}
+
+type MarkSellAsSolddUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    product: Product
+  }
+>
+
+export class MarkSellAsSolddUseCase {
+  constructor(
+    private productsRepository: ProductsRepository,
+    private sellersRepository: SellersRepository,
+  ) {}
+
+  async execute({
+    productId,
+    ownerId,
+  }: MarkSellAsSolddUseCaseRequest): Promise<MarkSellAsSolddUseCaseResponse> {
+    const seller = await this.sellersRepository.findById(ownerId)
+
+    if (!seller) {
+      return left(new ResourceNotFoundError('ownerId', ownerId))
+    }
+
+    const product = await this.productsRepository.findById(productId)
+
+    if (!product) {
+      return left(new ResourceNotFoundError('productId', productId))
+    }
+
+    if (product.onwer.id.toString() !== ownerId) {
+      return left(new NotAllowedError())
+    }
+
+    if (product.status === ProductStatus.CANCELLED) {
+      return left(new NotAllowedError())
+    }
+
+    product.status = ProductStatus.SOLD
+
+    this.productsRepository.save(product)
+
+    return right({
+      product,
+    })
+  }
+}
